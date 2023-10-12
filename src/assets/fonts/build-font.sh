26f99c3b327b91820922bb995d171f7ee03e6eb9
Timur Fayzrakhmanov <tim.fayzrakhmanov@gmail.com>
2023-10-12 16:18:47 +0300

    Add fonts

---
+++ src/assets/fonts/build-font.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "1. Subset the target TTF font to \"Hiper.\" chars at https://everythingfonts.com/subsetter"
+echo "2. Compress TTF to WOFF2 at https://everythingfonts.com/ttf-to-woff2"
