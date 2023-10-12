592cff647106b9cf3e96dd17b4353814e9791e5f
Timur Fayzrakhmanov <tim.fayzrakhmanov@gmail.com>
2023-10-12 17:31:28 +0300

    Update paths

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+OUT="docs"
 CNAME="src/CNAME"
 PAGES="src/{*.html,ru/**/*.html}"
 FAVICON="src/assets/icons/favicon.ico"
@@ -7,18 +8,18 @@ ICONS="src/assets/icons/*{.svg,.png}"
 FONTS="src/assets/fonts/*.woff2"
 MANIFEST="src/assets/*.webmanifest"
 STYLES="src/assets/styles/styles.css"
-STYLES_OUT="public/assets/styles.css"
+STYLES_OUT="$OUT/assets/styles.css"
 UTILS_OUT="src/assets/styles/utilities.css"
 PM="bunx"
 
 # Production build
 if [ "$1" = "--release" ]; then
-  bunx cpx "$CNAME" public
-  bunx cpx "$PAGES" public
-  bunx cpx "$FAVICON" public
-  bunx cpx "$ICONS" public/assets
-  bunx cpx "$FONTS" public/assets
-  bunx cpx "$MANIFEST" public/assets
+  bunx cpx "$CNAME" "$OUT"
+  bunx cpx "$PAGES" "$OUT"
+  bunx cpx "$FAVICON" "$OUT"
+  bunx cpx "$ICONS" "$OUT"/assets
+  bunx cpx "$FONTS" "$OUT"/assets
+  bunx cpx "$MANIFEST" "$OUT"/assets
   bunx unocss "$PAGES" --out-file "$UTILS_OUT" --preflights false
   bunx postcss "$STYLES" --output "$STYLES_OUT" --use postcss-import --use autoprefixer --autoprefixer '> 1%, last 5 versions' --use cssnano --no-map
   exit 0
@@ -26,12 +27,12 @@ fi
 
 # Development build
 $PM conc --kill-others \
-  "$PM cpx \"$CNAME\" public --watch" \
-  "$PM cpx \"$PAGES\" public --watch" \
-  "$PM cpx \"$FAVICON\" public --watch" \
-  "$PM cpx \"$ICONS\" public/assets --watch" \
-  "$PM cpx \"$FONTS\" public/assets --watch" \
-  "$PM cpx \"$MANIFEST\" public/assets --watch" \
+  "$PM cpx \"$CNAME\" $OUT --watch" \
+  "$PM cpx \"$PAGES\" $OUT --watch" \
+  "$PM cpx \"$FAVICON\" $OUT --watch" \
+  "$PM cpx \"$ICONS\" $OUT/assets --watch" \
+  "$PM cpx \"$FONTS\" $OUT/assets --watch" \
+  "$PM cpx \"$MANIFEST\" $OUT/assets --watch" \
   "$PM unocss \"$PAGES\" --out-file \"$UTILS_OUT\" --preflights false --watch" \
   "$PM postcss \"$STYLES\" --output \"$STYLES_OUT\" --use postcss-import --use autoprefixer --autoprefixer '> 1%, last 5 versions' --watch" \
-  "$PM web-dev-server --root-dir=public --watch"
+  "$PM web-dev-server --root-dir=$OUT --watch"
